e115986e7497172b851a7a14bc0dde2d5e97432b
kbs4674 <kbs4674@naver.com>
2019-12-09 15:36:00 +0000

    크롤링 중복검사 Colummn 추가

--- app/models/hit_product.rb
+++ app/models/hit_product.rb
@@ -1,4 +1,4 @@
 class HitProduct < ApplicationRecord
     attr_accessor :dateAgo, :shortDate, :uid, :isSoldOut, :imageUrl, :isDeleted
-    validates_uniqueness_of :url
+    validates_uniqueness_of :url, :scope => :title
 end
