39c1a1ddc4a54b3d4a4fed32b4252b356667ab84
kbs4674 <kbs4674@naver.com>
2019-12-14 11:25:42 +0000

    routes.rb 내용 정리

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,27 @@
 Rails.application.routes.draw do  
   root 'hit_products#index'
   
+  ## 앱 기본통신 URI
   get 'hit_products/index'
   get 'hit_products/condition'
   get 'hit_products/web_products_list' => "hit_products#web_products_list"
-  # get 'hit_products/clien'
-  # get 'hit_products/ppom'
-  # get 'hit_products/ruliweb'
-  # get 'hit_products/deal_ba_da'
   get 'hit_products/rank'
   get 'hit_products/search'
+  
+  ## JWT 토큰 생성 및 테스트
+  post 'auth_user' => 'authentication#authenticate_user'
+  get 'apis/test'
+  
+  ## 북마크
+  post 'apis/book_mark_combine'
+  post 'apis/book_mark_create'
+  delete 'apis/book_mark_destroy'
+  get 'apis/book_mark_list'
   
   resources :notices
   get '/notice.json' => 'notices#index_json'
 	
   devise_for :users
-  post 'auth_user' => 'authentication#authenticate_user'
-  get 'apis/test'
-  post 'apis/book_mark_combine'
-  post 'apis/book_mark_create'
-  delete 'apis/book_mark_destroy'
-  get 'apis/book_mark_list'
 	
   authenticate :user, lambda { |u| u.admin? } do
     begin
