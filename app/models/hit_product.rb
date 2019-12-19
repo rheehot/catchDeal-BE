ea1b7bf9de2339f3692f4b4f72c4f8572cbc711f
kbs4674 <kbs4674@naver.com>
2019-12-19 13:47:10 +0000

    변수명 등록 오류 수정

--- app/models/hit_product.rb
+++ app/models/hit_product.rb
@@ -1,5 +1,5 @@
 class HitProduct < ApplicationRecord
-    attr_accessor :dateAgo, :shortDate, :uid, :isSoldOut, :imageUrl, :isDeleted, :redirectUrl
+    attr_accessor :dateAgo, :shortDate, :uid, :isSoldOut, :imageUrl, :isDeleted, :shortUrl
     validates_uniqueness_of :url, :scope => :title
 	
 	has_many :book_marks
