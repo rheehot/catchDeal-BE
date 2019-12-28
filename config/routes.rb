4c4efec494e3aff1a83e5eea317c1335905535db
kbs4674 <kbs4674@naver.com>
2019-12-28 05:19:25 +0000

    북마크 라우터 설정 조정

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,30 @@ Rails.application.routes.draw do
   root 'hit_products#index'
   
   ## 앱 기본통신 URI
-  get 'hit_products/index'
-  get 'hit_products/condition'
+  get 'hit_products/index' => 'hit_products#index'
+  get 'hit-products/index' => 'hit_products#index'
+  
+  get 'hit_products/condition' => 'hit_products#condition'
+  get 'hit-products/condition' => 'hit_products#condition'
+  
   get 'hit_products/web_products_list' => "hit_products#web_products_list"
-  get 'hit_products/rank'
-  get 'hit_products/search'
+  get 'hit-products/web_products-list' => "hit_products#web_products_list"
+  
+  get 'hit_products/rank' => 'hit_products#rank'
+  get 'hit-products/rank' => 'hit_products#rank'
+  
+  get 'hit_products/search' => 'hit_products#search'
+  get 'hit-products/search' => 'hit_products#search'
   
   ## JWT 토큰 생성 및 테스트
   post 'auth_user' => 'authentication#authenticate_user'
   get 'apis/test' => "apis/test"
   
   ## 북마크
-  post 'apis/book_mark_combine'
-  post 'apis/book_mark_create'
-  delete 'apis/book_mark_destroy'
-  get 'apis/book_mark_list'
+  post 'apis/book_mark_combine' => 'apis#book_mark_combine'
+  post 'apis/book_mark_create' => 'apis#book_mark_create'
+  delete 'apis/book_mark_destroy' => 'apis#book_mark_destroy'
+  get 'apis/book_mark_list' => 'apis#book_mark_list'
   
   resources :notices
   get '/notice.json' => 'notices#index_json'
