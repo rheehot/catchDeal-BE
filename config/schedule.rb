5a2b0dbee9fd47907e2df741100b6cfeee21eae9
kbs4674 <kbs4674@naver.com>
2020-01-16 17:23:09 +0000

    스케쥴러 코드 수정

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,6 +18,6 @@ every 1.hours do
     command "cd /home/ubuntu/catch && hit_news_over_ruliweb_check:auto_collect RAILS_ENV=production; hit_news_over_clien_check:auto_collect RAILS_ENV=production; hit_news_over_deal_bada_check:auto_collect RAILS_ENV=production; hit_news_over_ppom_check:auto_collect RAILS_ENV=production;"
 end
 
-every 12.hours do
-    rake "rake chrome_check:auto_job", :environment => "production"
+every 3.hours do
+    rake "cd /home/ubuntu/catch && rake chrome_check:auto_job", :environment => "production"
 end
