3d22ccbe1fff165ef1afefd54173a0ab4a3f83c5
kbs4674 <kbs4674@naver.com>
2020-01-25 16:36:49 +0000

    기본 알람설정 수정

--- db/migrate/20200125080233_add_alarm_status_to_app_user.rb
+++ db/migrate/20200125080233_add_alarm_status_to_app_user.rb
@@ -1,5 +1,5 @@
 class AddAlarmStatusToAppUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :app_users, :alarm_status, :boolean, :null => false, :default => false
+    add_column :app_users, :alarm_status, :boolean, :null => false, :default => true
   end
 end
