5e0468f7eda984935c390309882d71f644689385
kbs4674 <kbs4674@naver.com>
2020-01-27 08:54:55 +0000

    database model file modified

--- db/migrate/20200127070750_create_keyword_pushalarm_lists.rb
+++ db/migrate/20200127070750_create_keyword_pushalarm_lists.rb
@@ -1,8 +1,8 @@
 class CreateKeywordPushalarmLists < ActiveRecord::Migration[5.2]
   def change
     create_table :keyword_pushalarm_lists do |t|
-      t.references :app_user
-      t.references :hit_product
+      t.belongs_to :app_user
+      t.belongs_to :hit_product
 
       t.timestamps
     end
