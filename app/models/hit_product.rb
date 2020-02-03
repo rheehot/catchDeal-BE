8dd84bae67151af48121024e744de9936762d5c3
kbs4674 <kbs4674@naver.com>
2020-02-03 10:15:30 +0000

    bookmark, keyword_pushalarm_lists, hit_products 간 Data 삭제 시 모델관계 설정

--- app/models/hit_product.rb
+++ app/models/hit_product.rb
@@ -2,9 +2,9 @@ class HitProduct < ApplicationRecord
     attr_accessor :productId, :dateAgo, :order, :isSoldOut, :imageUrl, :isDeleted, :shortUrl, :isTitleChanged
     validates_uniqueness_of :url, :scope => :title
 	
-	has_many :book_marks
+	has_many :book_marks, dependent: :destroy
     has_many :app_users, through: :book_marks
     
-    has_many :keyword_pushalarm_lists
+    has_many :keyword_pushalarm_lists, dependent: :destroy
     has_many :app_users, through: :keyword_pushalarm_lists
 end
